30aac18f81be60bdb0fe8def1afed4fde99fa476
GrodMaster <grodmaster2517@gmail.com>
2025-05-08 16:22:34 +0500

    add set and get Users

--- CodeBlogFintess.CMD/Program.cs
+++ CodeBlogFintess.CMD/Program.cs
@@ -10,25 +10,60 @@ namespace CodeBlogFintess.CMD
 
             Console.WriteLine("Вас приветствует приложение CodeBlogFitness.");
             
-            Console.WriteLine("Введите имя пользователя.");
+            Console.Write("Введите имя пользователя.");
             var name = Console.ReadLine();
 
-            Console.WriteLine("Введите пол.");
-            var gender = Console.ReadLine();
+            var userController = new UserController(name);
+            if (userController.IsNewUser)
+            {
+                Console.WriteLine("Введите пол.");
+                var gender = Console.ReadLine();
 
-            Console.WriteLine("Введите дату рождения.");
-            var birthdate = DateTime.Parse(Console.ReadLine()); //TODO: переписать TryParse
-
-            Console.WriteLine("Введите свой вес.");
-            var wight = double.Parse(Console.ReadLine());
-
-            Console.WriteLine("Введите свой рост.");
-            var height= double.Parse(Console.ReadLine());
+                var birthDate = ParsDateTime();
+                var weight = ParsDouble("Вес");
+                var height = ParsDouble("Рост");
 
 
-            var userController = new UserController(name, gender, birthdate, wight, height);
+                userController.SetNewUserData(gender, birthDate, weight, height);
 
-            userController.Save();
+            }
+            Console.WriteLine(userController.CurrentUser);
+
+        }
+
+        private static DateTime ParsDateTime()
+        {
+            DateTime birthDate;
+            while (true)
+            {
+                Console.Write("Введите дату рождения: dd.MM.yyyy : ");
+                if (DateTime.TryParse(Console.ReadLine(), out birthDate))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Неверный формат даты.");
+                }
+            }
+
+            return birthDate;
+        }
+
+        private static double ParsDouble(string name)
+        {
+            while (true)
+            {
+                Console.Write($"Введите {name} : ");
+                if (double.TryParse(Console.ReadLine(), out double value))
+                {
+                    return value;
+                }
+                else
+                {
+                    Console.WriteLine($"Неверный формат {name}.");
+                }
+            }
         }
     }
 }
---
+++ CodeBlogFitness.BL/Controller/UserController.cs
@@ -0,0 +1,99 @@
+﻿using CodeBlogFitness.BL.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace CodeBlogFintess.CMD.Controller
+{
+    /// <summary>
+    /// Контроллер пользователя.
+    /// </summary>
+    public class UserController
+    {
+        /// <summary>
+        /// Пользователь приложения.
+        /// </summary>
+        public List<User>  Users { get; }
+        
+        /// <summary>
+        /// Текущий пользователь.
+        /// </summary>
+        public User CurrentUser { get; }
+
+        public bool IsNewUser { get; } = false;
+
+        /// <summary>
+        /// Создание нового контроллера.
+        /// </summary>
+        /// <param name="user">Пользователь.</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public UserController(string userName)
+        {
+            if (string.IsNullOrWhiteSpace(userName))
+            {
+                throw new ArgumentNullException("Имя пользователя не можеть быть пустым", nameof(userName));
+            }
+
+            Users = GetUsersData();
+
+            CurrentUser = Users.SingleOrDefault(user => user.Name == userName);
+
+            if (CurrentUser == null)
+            {
+                CurrentUser = new User(userName);
+                Users.Add(CurrentUser);
+                IsNewUser = true;
+                Save();
+            }
+        }
+
+        public void SetNewUserData(string genderName, DateTime birthDate, double wight = 1, double height = 1)
+        {
+            // TODO: проверка
+            CurrentUser.Gender = new Gender(genderName);
+            CurrentUser.BirthDate = birthDate;
+            CurrentUser.Weight = wight;
+            CurrentUser.Height = height;
+
+            Save();
+        }
+
+        /// <summary>
+        /// Получить сохраненный список пользователей.
+        /// </summary>
+        /// <returns>Список пользователей приложения.</returns>
+        /// <exception cref="FileLoadException"></exception>
+        private List<User> GetUsersData()
+        {
+            var formatter = new BinaryFormatter();
+            using (var file = new FileStream("users.dat", FileMode.OpenOrCreate))
+            {
+                if (formatter.Deserialize(file) is List<User> users)
+                {
+                    return users;
+                }
+                else
+                {
+                    return new List<User>();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Сохранить данные пользователя.
+        /// </summary>
+        /// <exception cref="Exception"></exception>
+        private void Save()
+        {
+            var formatter = new BinaryFormatter();
+                using(var file = new FileStream("users.dat", FileMode.OpenOrCreate))
+                {
+                    formatter.Serialize(file, Users);
+                }
+        }
+
+      
+    }
+}
